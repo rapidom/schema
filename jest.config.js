376e515eee3932f7d3969c89dec11e212bcfd6e4
Ardalan Amini <ardalanamini22@gmail.com>
2022-05-22 19:44:25 +0430

    deps(Dev): bump jest from 27.0.4 to 28.1.0

--- jest.config.js
+++ jest.config.js
@@ -101,7 +101,7 @@ module.exports = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: ["default", "github-actions"],
 
   // Automatically reset mock state between every test
   // resetMocks: false,
